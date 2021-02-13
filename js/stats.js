dffdda38f62c0da5109d79fabdc7942f96d1839a
hwchw316 <hwchw316@gmail.com>
2021-02-13 08:01:54 +0000

    got stuck with axios api get git status

---
+++ js/stats.js
@@ -0,0 +1,15 @@
+let apikey_alphavantage = '5b7fc71725msh6d5485b5eebd700p173114jsn9573a5c553e8';
+let searchbtn = document.querySelector('#search-btn');
+let searchinput  = document.querySelector('#searchinput').value;
+
+searchbtn.addEventListener('click',async function(){
+    let response = await axios.get ('https://www.alphavantage.co/query', {
+       'params': {
+           'function': 'BALANCE_SHEET',
+           'symbol' : searchinput,
+           'apikey' : '5b7fc71725msh6d5485b5eebd700p173114jsn9573a5c553e8'
+       }
+    })
+       console.log(response.data);   
+});
+
