5d9eac1acf9d789725f4642edab026bcc0395a64
hwchw316 <hwchw316@gmail.com>
2021-02-14 09:55:49 +0000

    added framwork for stats

--- js/stats.js
+++ js/stats.js
@@ -1,8 +1,9 @@
 let apikey_alphavantage = '5b7fc71725msh6d5485b5eebd700p173114jsn9573a5c553e8';
 let searchbtn = document.querySelector('#search-btn');
-let searchinput  = document.querySelector('#searchinput').value;
+// let searchinput  = document.querySelector('#searchinput').value;
 
 searchbtn.addEventListener('click',async function(){
+    let searchinput  = document.querySelector('#searchinput').value;
     let response = await axios.get ('https://www.alphavantage.co/query', {
        'params': {
            'function': 'BALANCE_SHEET',
@@ -12,4 +13,3 @@ searchbtn.addEventListener('click',async function(){
     })
        console.log(response.data);   
 });
-
