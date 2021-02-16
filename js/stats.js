82ab442f6d10046661bad5bcc24098e6fa5c3c32
hwchw316 <hwchw316@gmail.com>
2021-02-16 18:57:25 +0000

    midst of adding function

--- js/stats.js
+++ js/stats.js
@@ -2,15 +2,25 @@ let apikey_alphavantage = '5b7fc71725msh6d5485b5eebd700p173114jsn9573a5c553e8';
 let searchbtn = document.querySelector('#search-btn');
 // let searchinput  = document.querySelector('#searchinput').value;
 
-searchbtn.addEventListener('click',async function(){
-    let searchinput  = document.querySelector('#searchinput').value;
-    console.log(searchinput);
-    let response = await axios.get ('https://www.alphavantage.co/query', {
-       'params': {
-           'function': 'BALANCE_SHEET',
-           'symbol' : searchinput,
-           'apikey' : '5b7fc71725msh6d5485b5eebd700p173114jsn9573a5c553e8'
-       }
+searchbtn.addEventListener('click', async function () {
+    let searchinput = document.querySelector('#searchinput').value;
+    let response = await axios.get('https://www.alphavantage.co/query', {
+        'params': {
+            'function': 'BALANCE_SHEET',
+            'symbol': searchinput,
+            'apikey': '5b7fc71725msh6d5485b5eebd700p173114jsn9573a5c553e8'
+        }
     })
-       console.log(response.data);   
+    let mysymbol= response.data.symbol;
+    console.log(mysymbol);
+
+      = document.querySelector('stocksymbol')
+
+    // let stocksymbol = document.querySelector('#stocksymbol');
+    // let stocksymbolelement = document.createElement('sse');
+    // stocksymbolelement.innerHTML = response.data.symbol;
+    // stocksymbol.appendChild(stocksymbolelement);
+    // console.log(response.data.symbol);
+    
 });
+
