392ade030cc5222d5eaa54853417e91915e3a7fc
hwchw316 <hwchw316@gmail.com>
2021-02-17 18:24:46 +0000

    trying to add buttons that changes chart timing

--- js/candle.js
+++ js/candle.js
@@ -14,7 +14,7 @@ var dataarray = [];
 //     data: dataaaray
 // }])
 
-
+// daily
 function loadChart() {
     //apex candlestick chart
     var options = {
@@ -44,9 +44,39 @@ function loadChart() {
     console.log("The Chart is loaded");
 }
 
-// let apikey_finnhub =  'c0m9hnn48v6rkav1u130'
-let searchbtn2 = document.querySelector('#search-btn');
 
+
+// // 1 minute
+// function loadChart1() {
+//     //apex candlestick chart
+//     var options = {
+//         series: [{
+//             data: dataarray
+//         }],
+//         chart: {
+//             type: 'candlestick',
+//             height: 350
+//         },
+//         title: {
+//             text: '1 Minute Chart',
+//             align: 'left'
+//         },
+//         xaxis: {
+//             type: 'datetime'
+//         },
+//         yaxis: {
+//             tooltip: {
+//                 enabled: true
+//             }
+//         }
+//     };
+
+//     var chart = new ApexCharts(document.querySelector("#candlestick"), options);
+//     chart.render();
+//     console.log("The 1 minute Chart is loaded");
+// }
+
+/// main search bar
 searchbtn.addEventListener('click', async function () {
     dataarray = [];
     let searchinput = document.querySelector('#searchinput').value;
@@ -86,3 +116,42 @@ searchbtn.addEventListener('click', async function () {
     loadChart();
 });
 
+// ///  1 minute chart 
+// btn1min.addEventListener('click', async function () {
+//     dataarray = [];
+//     let searchinput = document.querySelector('#searchinput').value;
+//     let response = await axios.get('https://www.alphavantage.co/query', {
+//         'params': {
+//             'function': 'TIME_SERIES_DAILY',
+//             'symbol': searchinput,
+//             'interval': '1min',
+//             'apikey': '5b7fc71725msh6d5485b5eebd700p173114jsn9573a5c553e8'
+//         }
+//     })
+//     console.log(response.data);
+//     console.log(response.data["Time Series (1min)"]);
+//     let dataloaded = response.data["Time Series (1min)"];
+//     // change data's date to UNIX
+
+//     for (let date in dataloaded) {
+
+//         let open = dataloaded[date]['1. open'];
+//         let high = dataloaded[date]['2. high'];
+//         let low = dataloaded[date]['3. low'];
+//         let close = dataloaded[date]['4. close'];
+//         let y = [open, high, low, close];
+//         var myDate = new Date(date); // Your timezone!
+//         var myEpoch = myDate.getTime();
+//         console.log(myEpoch);
+//         // change date format
+//         let item = {
+//             x: new Date(myEpoch),
+//             y: [open, high, low, close]
+//         }
+//         dataarray.unshift(item);
+//     }
+
+//     console.log(dataarray);
+
+//     loadChart1();
+// });
