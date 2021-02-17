6c3664069cf62c05b3a727aae8f33a74e5a801b1
hwchw316 <hwchw316@gmail.com>
2021-02-17 03:19:29 +0000

    updated researchhtml and js to call details of stock

--- js/stats.js
+++ js/stats.js
@@ -1,7 +1,8 @@
-let apikey_alphavantage = '5b7fc71725msh6d5485b5eebd700p173114jsn9573a5c553e8';
+// let apikey_alphavantage = '5b7fc71725msh6d5485b5eebd700p173114jsn9573a5c553e8';
 let searchbtn = document.querySelector('#search-btn');
 // let searchinput  = document.querySelector('#searchinput').value;
 
+//// Alpha Vantage Balance Sheet
 searchbtn.addEventListener('click', async function () {
     let searchinput = document.querySelector('#searchinput').value;
     let response = await axios.get('https://www.alphavantage.co/query', {
@@ -12,15 +13,32 @@ searchbtn.addEventListener('click', async function () {
         }
     })
     let mysymbol= response.data.symbol;
-    console.log(mysymbol);
-
-      = document.querySelector('stocksymbol')
-
-    // let stocksymbol = document.querySelector('#stocksymbol');
-    // let stocksymbolelement = document.createElement('sse');
-    // stocksymbolelement.innerHTML = response.data.symbol;
-    // stocksymbol.appendChild(stocksymbolelement);
-    // console.log(response.data.symbol);
-    
+    // console.log(mysymbol);
+    document.getElementById('stocksymbol').innerHTML = mysymbol.toString();
+    // console.log(response.data);
 });
+
+
+//// Alpha Vantage profile
+searchbtn.addEventListener('click', async function () {
+    let searchinput = document.querySelector('#searchinput').value;
+    let response = await axios.get('https://www.alphavantage.co/query', {
+        'params': {
+            'function': 'OVERVIEW',
+            'symbol': searchinput,
+            'apikey': '5b7fc71725msh6d5485b5eebd700p173114jsn9573a5c553e8'
+        }
+    })
+    let CompanyName= response.data.Name;
+    let CompanyDes = response.data.Description;
+    let CompanyIndustry = response.data.Industry;
+    let CompanySector = response.data.Sector;
+    // console.log(mysymbol);
+    document.getElementById('sector1').innerHTML = CompanySector;
+    document.getElementById('industry1').innerHTML = CompanyIndustry;
+    document.getElementById('stockname').innerHTML = CompanyName;
+    document.getElementById('description1').innerHTML = CompanyDes;
+    // console.log(response.data);
+});
+
 
