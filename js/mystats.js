3facf9a806f1e103ece8709403131f9d17028d80
hwchw316 <hwchw316@gmail.com>
2021-02-19 06:08:01 +0000

    updated windows storage

---
+++ js/mystats.js
@@ -0,0 +1,11 @@
+let updateinitialbtn = document.querySelector('#initialamtbtn');
+storedinitial = JSON.parse(window.localStorage.getItem('storedinitial'));
+document.getElementById('statsinitial').innerHTML = storedinitial ;
+
+updateinitialbtn.addEventListener('click', function () {
+    let updateinitial = document.querySelector('#initialamounttext').value;
+    console.log (updateinitial);
+    document.getElementById('statsinitial').innerHTML = updateinitial ;
+    localStorage.setItem("storedinitial", updateinitial);
+    
+});
