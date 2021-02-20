0e4f1954b210b51038cd9f4f9217e96548f85188
hwchw316 <hwchw316@gmail.com>
2021-02-20 12:42:20 +0000

    updated stats to display live pnl changes

--- js/stats.js
+++ js/stats.js
@@ -2,17 +2,23 @@ let updateinitialbtn = document.querySelector('#initialamtbtn');
 storedinitial = JSON.parse(window.localStorage.getItem('storedinitial'));
 document.getElementById('statsinitial').innerHTML = storedinitial;
 
-
 let curramtbtn = document.querySelector('#curramtbtn');
 storedcurr = JSON.parse(window.localStorage.getItem('storedcurr'));
 document.getElementById('statscurr').innerHTML = storedcurr;
+
+let storedpnl = parseInt(storedcurr) - parseInt(storedinitial);
+document.getElementById('PNL').innerHTML = storedcurr;
+
+storedcurr = JSON.parse(window.localStorage.getItem('storedpnl'));
+document.getElementById('PNL').innerHTML = storedpnl;
+
 
 updateinitialbtn.addEventListener('click', function () {
     let updateinitial = document.querySelector('#initialamounttext').value;
     console.log(updateinitial);
     document.getElementById('statsinitial').innerHTML = updateinitial;
     localStorage.setItem("storedinitial", updateinitial);
-
+    location.reload();
 });
 
 curramtbtn.addEventListener('click', function () {
@@ -20,7 +26,7 @@ curramtbtn.addEventListener('click', function () {
     console.log(updateinitial);
     document.getElementById('statscurr').innerHTML = updateinitial;
     localStorage.setItem("storedcurr", updateinitial);
+    location.reload();
 });
-
 
 
